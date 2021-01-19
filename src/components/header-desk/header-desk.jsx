aa33a0746caaff4a25c798262f3ac3b80bd3b3f8
rsdsantis <rsdsantis@gmail.com>
2021-01-18 21:44:47 -0300

    Feat: header desk

--- src/components/header-desk/header-desk.jsx
+++ src/components/header-desk/header-desk.jsx
@@ -1,22 +1,45 @@
 import React from "react";
 import {
   StyledHeader,
+  StyledUpperContent,
+  StyledLogo,
+  StyledAdsDiv,
+  StyledPublishButton,
+  StyledNavBar,
   StyledLeftWing,
   StyledRightWing,
   StyledSearchDiv,
   StyledSearchInput,
   StyledSearchImg,
+  StyledNavLink,
 } from "./header-desk.js";
 
 const Header = () => {
   return (
     <StyledHeader>
-      <StyledLeftWing />
-      <StyledSearchDiv>
-        <StyledSearchInput />
-        <StyledSearchImg />
-      </StyledSearchDiv>
-      <StyledRightWing />
+      <StyledUpperContent>
+        <StyledLogo />
+        <StyledAdsDiv />
+        <StyledPublishButton>Publish Now</StyledPublishButton>
+      </StyledUpperContent>
+      <StyledNavBar>
+        <StyledLeftWing>
+          <StyledNavLink>Home</StyledNavLink>
+          <StyledNavLink>Hot News</StyledNavLink>
+          <StyledNavLink>Trends</StyledNavLink>
+          <StyledNavLink>Hardware</StyledNavLink>
+        </StyledLeftWing>
+        <StyledSearchDiv>
+          <StyledSearchInput />
+          <StyledSearchImg />
+        </StyledSearchDiv>
+        <StyledRightWing>
+          <StyledNavLink>Security</StyledNavLink>
+          <StyledNavLink>Gaming</StyledNavLink>
+          <StyledNavLink>Ranking</StyledNavLink>
+          <StyledNavLink>Profile</StyledNavLink>
+        </StyledRightWing>
+      </StyledNavBar>
     </StyledHeader>
   );
 };
